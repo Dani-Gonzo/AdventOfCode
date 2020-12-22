e899dc935f915700bdc08a0ae75738761b56df2c
Danielle L <d.gonso@hotmail.com>
2020-12-21 16:35:32 -0800

    Initial commit

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using AdventOfCode.Solutions;
+
+namespace AdventOfCode
+{
+
+    class Program
+    {
+
+        public static Config Config = Config.Get("config.json");
+        static SolutionCollector Solutions = new SolutionCollector(Config.Year, Config.Days);
+
+        static void Main(string[] args) {
+            long total = 0;
+            foreach( ASolution solution in Solutions ) {
+                solution.Solve();
+                total += solution.ContructionTime + solution.Part1Ticks + solution.Part2Ticks;
+            }
+            string output = $"Total time taken: {TimeSpan.FromTicks(total).TotalMilliseconds}ms | {total} ticks | {TimeSpan.FromTicks(total)}";
+            Trace.WriteLine(output);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/SolutionCollector.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace AdventOfCode.Solutions
+{
+
+    class SolutionCollector : IEnumerable<ASolution>
+    {
+
+        IEnumerable<ASolution> Solutions;
+
+        public SolutionCollector(int year, int[] days) => Solutions = LoadSolutions(year, days).ToArray();
+
+        public ASolution GetSolution(int day) {
+            try {
+                return Solutions.Single(s => s.Day == day);
+            }
+            catch( InvalidOperationException ) {
+                return null;
+            }
+        }
+
+        public IEnumerator<ASolution> GetEnumerator() {
+            return Solutions.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        IEnumerable<ASolution> LoadSolutions(int year, int[] days) {
+            if( days.Sum() == 0 ) {
+                days = Enumerable.Range(1, 25).ToArray();
+            }
+
+            foreach( int day in days ) {
+                var solution = Type.GetType($"AdventOfCode.Solutions.Year{year}.Day{day.ToString("D2")}");
+                if( solution != null ) {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    ASolution val = (ASolution)Activator.CreateInstance(solution);
+                    sw.Stop();
+                    val.ContructionTime = sw.ElapsedTicks;
+                    yield return val;
+                }
+            }
+        }
+    }
+}
