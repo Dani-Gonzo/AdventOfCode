ff788413c82547a459cba93090c8ccb8bad135cb
Book Dragon <d.gonso@hotmail.com>
2020-12-22 20:11:18 -0800

    Day 1 and 2

--- AdventOfCode/Solutions/Year2020/Day01-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01-Solution.cs
@@ -1,25 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day01 : ASolution
     {
+        int[] ParsedInput;
+        int sum = 2020;
 
         public Day01() : base(01, 2020, "")
         {
-
+            UseDebugInput = false;
+            string[] SplitInput = Input.Split("\n");
+            ParsedInput = SplitInput.Select(int.Parse).ToArray();
         }
 
         protected override string SolvePartOne()
         {
+            int num1;
+            int num2;
+            for (int i = 0; i < ParsedInput.Length; i++) {
+                num1 = ParsedInput[i];
+                for (int j = 1; j < ParsedInput.Length; j++) {
+                    num2 = ParsedInput[j];
+                    if (num1 + num2 == sum)
+                    {
+                        int final = num1 * num2;
+                        return final.ToString();
+                    }
+                }
+            }
+            
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            int num1, num2, num3;
+            for (int i = 0; i < ParsedInput.Length; i++) {
+                num1 = ParsedInput[i];
+                for (int j = i+1; j < ParsedInput.Length; j++) {
+                    num2 = ParsedInput[j];
+                    for (int k = j + 1; k < ParsedInput.Length; k++) {
+                        num3 = ParsedInput[k];
+                        if (num1 + num2 + num3 == sum)
+                        {
+                            int product = num1 * num2 * num3;
+                            return product.ToString();
+                        }
+                    }
+                }
+            }
             return null;
         }
     }
--- AdventOfCode/Solutions/Year2020/Day02-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02-Solution.cs
@@ -4,23 +4,56 @@ using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
 {
+    class Rule {
+        public int minNum;
+        public int maxNum;
+        public char rule;
+        public string password;
+    }
 
     class Day02 : ASolution
     {
+        List<Rule> ParsedInput = new List<Rule>();
 
         public Day02() : base(02, 2020, "")
         {
-
+            UseDebugInput = false;
+            string[] SplitInput = Input.Split("\n");
+            foreach (string element in SplitInput) {
+                string[] pieces = element.Split(new char[] { '-', ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                var ruleEntry = new Rule { minNum = Int32.Parse(pieces[0]), maxNum = Int32.Parse(pieces[1]), rule = char.Parse(pieces[2]), password = pieces[3] };
+                ParsedInput.Add(ruleEntry);
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int validPasswords = 0;
+            ParsedInput.ForEach(item => {
+                int validChars = 0;
+                foreach (char c in item.password) {
+                    if (c == item.rule) {
+                        validChars++;
+                    }
+                }
+                if (validChars >= item.minNum && validChars <= item.maxNum) {
+                    validPasswords++;
+                }
+            });
+            return validPasswords.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int validPasswords = 0;
+            ParsedInput.ForEach(item => {
+                //int validChars = 0;
+                if (item.password[item.minNum-1] == item.rule ^ item.password[item.maxNum-1] == item.rule) {
+                    validPasswords++;
+                }
+            });
+
+            return validPasswords.ToString();
         }
     }
 }
