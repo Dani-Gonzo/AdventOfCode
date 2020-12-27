36c6e7f3acb8d9f4057327d54ea82a86b3f78448
Book Dragon <d.gonso@hotmail.com>
2020-12-26 20:23:07 -0800

    Day 4 and 5

--- AdventOfCode/Solutions/Year2020/Day04-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04-Solution.cs
@@ -1,26 +1,110 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day04 : ASolution
     {
-
+        string[] ParsedInput;
         public Day04() : base(04, 2020, "")
         {
-
+            UseDebugInput = false;
+            string[] SplitInput = Input.Split(new string[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+            ParsedInput = SplitInput;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int validPassports = 0;
+            for (int i = 0; i < ParsedInput.Length; i++) {
+                if (ParsedInput[i].Contains("byr") &&
+                ParsedInput[i].Contains("iyr") &&
+                ParsedInput[i].Contains("eyr") &&
+                ParsedInput[i].Contains("hgt") &&
+                ParsedInput[i].Contains("hcl") &&
+                ParsedInput[i].Contains("ecl") &&
+                ParsedInput[i].Contains("pid")) {
+                    validPassports++;
+                }
+            }
+            return validPassports.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+
+            int validPasswords = 0;
+
+            for (int i = 0; i < ParsedInput.Length; i++)
+            {
+                if (ParsedInput[i].Contains("byr") &&
+                ParsedInput[i].Contains("iyr") &&
+                ParsedInput[i].Contains("eyr") &&
+                ParsedInput[i].Contains("hgt") &&
+                ParsedInput[i].Contains("hcl") &&
+                ParsedInput[i].Contains("ecl") &&
+                ParsedInput[i].Contains("pid"))
+                {
+                    var SplitInput = ParsedInput[i].Split(new char[] { '\n', ' ' }).Select(element => element.Split(':')).ToDictionary(x => x[0], x => x[1]);
+                    int byr = int.Parse(SplitInput["byr"]);
+                    int iyr = int.Parse(SplitInput["iyr"]);
+                    int eyr = int.Parse(SplitInput["eyr"]);
+                    int hgt;
+
+                    if (!(byr >= 1920 && byr <= 2002)) {
+                        continue;
+                    }
+
+                    if (!(iyr >= 2010 && iyr <= 2020)) {
+                        continue;
+                    }
+
+                    if (!(eyr >= 2020 && eyr <= 2030)) {
+                        continue;
+                    }
+
+                    if (SplitInput["hgt"].Contains("in")) {
+                        hgt = int.Parse(SplitInput["hgt"].Trim('i', 'n'));
+                        if (!(hgt >= 59 && hgt <= 76)) {
+                            continue;
+                        }
+                    } else if (SplitInput["hgt"].Contains("cm")) {
+                        hgt = int.Parse(SplitInput["hgt"].Trim('c', 'm'));
+                        if (!(hgt >= 150 && hgt <= 193))
+                        {
+                            continue;
+                        }
+                    } else {
+                        continue;
+                    }
+
+                    if (!(Regex.IsMatch(SplitInput["hcl"], "^#[a-f0-9]{6}"))) {
+                        continue;
+                    }
+
+                    if (!(SplitInput["ecl"] == "amb" |
+                    SplitInput["ecl"] == "blu" |
+                    SplitInput["ecl"] == "brn" |
+                    SplitInput["ecl"] == "gry" |
+                    SplitInput["ecl"] == "grn" |
+                    SplitInput["ecl"] == "hzl" |
+                    SplitInput["ecl"] == "oth")) {
+                        continue;
+                    }
+
+                    if (!(Regex.IsMatch(SplitInput["pid"], "^\\d{9}$"))) {
+                        continue;
+                    }
+
+                    validPasswords++;
+                }
+            }
+            return validPasswords.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day05-Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05-Solution.cs
@@ -7,20 +7,95 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day05 : ASolution
     {
-
+        string[] SplitInput;
         public Day05() : base(05, 2020, "")
         {
-
+            UseDebugInput = false;
+            SplitInput = Input.Split("\n");
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            int minRow = 0;
+            int maxRow = 127;
+            int minSeat = 0;
+            int maxSeat = 7;
+
+            int prevId = 0;
+
+            for (int i = 0; i < SplitInput.Length; i++) {
+                minRow = 0;
+                maxRow = 127;
+                minSeat = 0;
+                maxSeat = 7;
+
+                foreach (char c in SplitInput[i]) {
+                    if (c == 'F') {
+                        maxRow = ((maxRow - minRow) / 2 + minRow);
+                    } else if (c == 'B') {
+                        minRow = ((maxRow - minRow) / 2 + minRow + 1);
+                    } else if (c == 'R') {
+                        minSeat = ((maxSeat - minSeat) / 2 + minSeat + 1);
+                    } else if (c == 'L') {
+                        maxSeat = ((maxSeat - minSeat) / 2 + minSeat);
+                    }
+                }
+
+                int nextId = (minRow * 8) + minSeat;
+                if (nextId > prevId) {
+                    prevId = nextId;
+                }
+            }
+
+            return prevId.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            List<int> id = new List<int>();
+            int minRow = 0;
+            int maxRow = 127;
+            int minSeat = 0;
+            int maxSeat = 7;
+
+            for (int i = 0; i < SplitInput.Length; i++)
+            {
+                minRow = 0;
+                maxRow = 127;
+                minSeat = 0;
+                maxSeat = 7;
+
+                foreach (char c in SplitInput[i])
+                {
+                    if (c == 'F')
+                    {
+                        maxRow = ((maxRow - minRow) / 2 + minRow);
+                    }
+                    else if (c == 'B')
+                    {
+                        minRow = ((maxRow - minRow) / 2 + minRow + 1);
+                    }
+                    else if (c == 'R')
+                    {
+                        minSeat = ((maxSeat - minSeat) / 2 + minSeat + 1);
+                    }
+                    else if (c == 'L')
+                    {
+                        maxSeat = ((maxSeat - minSeat) / 2 + minSeat);
+                    }
+                }
+                id.Add((minRow * 8) + minSeat);
+            }
+            id.Sort();
+            int mySeat = 0;
+            for (int i = 0; i < id.Count-1; i++) {
+                if (id[i+1] - id[i] > 1) {
+                    int highSeat = id[i + 1];
+                    mySeat = highSeat - 1;
+                }
+            }
+
+            return mySeat.ToString();
         }
     }
 }
